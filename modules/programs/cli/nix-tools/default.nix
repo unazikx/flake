11a790b3fdb849aeade42312308ab682107ab81d
unazikx <xfalwa@gmail.com>
2026-04-20 00:19:46 +0900

    nix-config: change clear

--- modules/programs/cli/nix-tools/default.nix
+++ modules/programs/cli/nix-tools/default.nix
@@ -44,7 +44,16 @@
 
           programs.nh = {
             enable = true;
-            clean.enable = true;
+
+            clean = {
+              enable = true;
+              extraArgs = lib.concatStringsSep " " [
+                "--keep 3"
+                "--keep-since 1week"
+                "--optimise"
+              ];
+            };
+
             flake = lib.flakeDir;
           };
         };
