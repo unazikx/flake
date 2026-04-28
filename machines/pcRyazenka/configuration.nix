a2617688f8ff3b9f3e27644af38f9ed33a80f086
unazikx <xfalwa@gmail.com>
2026-04-29 08:30:13 +0900

    base16: init

--- machines/pcRyazenka/configuration.nix
+++ machines/pcRyazenka/configuration.nix
@@ -77,26 +77,7 @@
                 stylix = {
                   polarity = "dark";
 
-                  base16Scheme = {
-                    name = "Grayscale Dark";
-                    author = "https://github.com/Alexx2";
-                    base00 = "#101010";
-                    base01 = "#252525";
-                    base02 = "#464646";
-                    base03 = "#525252";
-                    base04 = "#ababab";
-                    base05 = "#b9b9b9";
-                    base06 = "#e3e3e3";
-                    base07 = "#f7f7f7";
-                    base08 = "#7c7c7c";
-                    base09 = "#999999";
-                    base0A = "#a0a0a0";
-                    base0B = "#8e8e8e";
-                    base0C = "#868686";
-                    base0D = "#686868";
-                    base0E = "#747474";
-                    base0F = "#5e5e5e";
-                  };
+                  base16Scheme = import ../../modules/themes/base16/paradise.nix;
 
                   image = lib.mkStylixImage (pkgs.fetchurl {
                     url = "https://w.wallhaven.cc/full/og/wallhaven-ogl5z9.png";
