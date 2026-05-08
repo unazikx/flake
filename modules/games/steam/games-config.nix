c59e2fac2c99ce0e16496f9acdf95b292b146d2d
unazikx <xfalwa@gmail.com>
2026-05-08 09:53:31 +0900

    steam: add more games

--- modules/games/steam/games-config.nix
+++ modules/games/steam/games-config.nix
@@ -28,17 +28,19 @@ in
         };
       }
       {
-        duck-game = {
-          # https://www.protondb.com/app/312530#VBbR6m5Ilw
-          compatTool = "proton_8";
-          id = 312530;
-        };
+        # keep-sorted start block=yes
 
         city-car-driving = {
           compatTool = "proton_8";
           id = 493490;
         };
+        duck-game = {
+          # https://www.protondb.com/app/312530#VBbR6m5Ilw
+          compatTool = "proton_8";
+          id = 312530;
+        };
       }
+      # keep-sorted end
     )
 
     # default proton tool
@@ -51,49 +53,51 @@ in
         };
       }
       {
-        terraria = {
-          id = 105600;
-        };
-
-        project-zomboid = {
-          id = 108600;
-        };
-
-        valheim = {
-          id = 892970;
-        };
-
-        lucid-blocks = {
-          id = 3495730;
-        };
-
-        sandbox = {
-          id = 590830;
-        };
-
-        counter-strike-cz = {
-          id = 80;
-        };
-
-        repo = {
-          id = 3214660;
-        };
-
-        worms-wmd = {
-          id = 327030;
-        };
+        # keep-sorted start block=yes
 
         citizen-sleeper = {
           id = 1578650;
         };
-
         citizen-sleeper-2 = {
           id = 2442460;
         };
-
+        counter-strike-cz = {
+          id = 80;
+        };
         darksiders-genesis = {
           id = 710920;
         };
+        lucid-blocks = {
+          id = 3495730;
+        };
+        noita = {
+          id = 881100;
+        };
+        one-million-fatal-guns = {
+          id = 155710;
+        };
+        plastic-warfare = {
+          id = 490640;
+        };
+        project-zomboid = {
+          id = 108600;
+        };
+        repo = {
+          id = 3214660;
+        };
+        sandbox = {
+          id = 590830;
+        };
+        terraria = {
+          id = 105600;
+        };
+        valheim = {
+          id = 892970;
+        };
+        worms-wmd = {
+          id = 327030;
+        };
+        # keep-sorted end
       }
     )
   ];
@@ -103,12 +107,13 @@ in
       (lib.steam.mkAttrset
         {
           enable = lib.mkDefault false; # by default disabled
-          compatTool = defaultCompatTool;
-          launchOptions.env = {
-            STEAM_COMPAT_DATA_PATH = "${steamapps}/compatdata/0/pfx";
+          compatTool = lib.mkDefault defaultCompatTool;
+          launchOptions = {
+            env.STEAM_COMPAT_DATA_PATH = "${steamapps}/compatdata/0/pfx";
           };
         }
         {
+          # keep-sorted start block=yes
           voices-of-the-void = {
             name = "Voices of the Void";
             target = lib.getExe pkgs.own.games.votv;
@@ -137,6 +142,37 @@ in
               };
             };
           };
+
+          super-tux-kart = {
+            name = "Super Tux Kart";
+            target = lib.getExe pkgs.supertuxkart;
+            compatTool = null;
+
+            icon = pkgs.fetchurl {
+              url = "https://cdn2.steamgriddb.com/icon_thumb/7a576629fef88f3e636afd33b09e8289.png";
+              sha256 = "sha256-njzkz9L0cvWH/RhrWf3mzMbJ7nnRvQgpbmqJyq8p8uQ=";
+            };
+
+            artwork = {
+              logo = pkgs.fetchurl {
+                url = "https://cdn2.steamgriddb.com/logo_thumb/1cfdf3e390adc43a235bedda982b66b5.png";
+                sha256 = "sha256-UKj0dgTNqFP1uI9CqCVbtgrY67aMj4I837xDv4xYiWY=";
+              };
+              hero = pkgs.fetchurl {
+                url = "https://cdn2.steamgriddb.com/hero_thumb/76fdca9cd791acce9582bb354b48e608.jpg";
+                sha256 = "sha256-quUzsDzt9RgsDtcxyfbSGQj95KcDTD5iWSkWsPpZYuk=";
+              };
+              banner = pkgs.fetchurl {
+                url = "https://cdn2.steamgriddb.com/thumb/168f6513fd770f0a1e48666f60950251.jpg";
+                sha256 = "sha256-cktay3D7jqdK7SMKT9Q8wfza9c2c6ZLZnprvMZr15XQ=";
+              };
+              cover = pkgs.fetchurl {
+                url = "https://cdn2.steamgriddb.com/thumb/27d2cb1a3963d82eec5cfddc21d15fd9.jpg";
+                sha256 = "sha256-EmO48QR43Bnz9/kjohR9unRoHj8DVTOs89iEUWX3wI8=";
+              };
+            };
+          };
+          # keep-sorted end
         }
       )
     ]
