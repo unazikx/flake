8198d75c893b18112d7c21b71d9575afbd7a1833
unazikx <xfalwa@gmail.com>
2026-06-19 11:40:11 +0900

    bluetooth: idk ctrl-v

--- modules/hardware/bluetooth/default.nix
+++ modules/hardware/bluetooth/default.nix
@@ -25,13 +25,16 @@
 
           hardware.bluetooth = {
             enable = true;
-
             powerOnBoot = true;
 
             settings = {
               General = {
                 Experimental = true;
                 Enable = "Source,Sink,Media,Socket";
+                Privacy = "device";
+                JustWorksRepairing = "always";
+                Class = "0x000100";
+                FastConnectable = true;
               };
 
               LE = {
