6e49e191f4791a5275be931e9bd13aab1d436e8b
unazikx <xfalwa@gmail.com>
2026-07-24 21:05:06 +0900

    jetpure: disable module

--- configurations/jetpure/configuration.nix
+++ configurations/jetpure/configuration.nix
@@ -85,7 +85,6 @@
       zen.programs.cli.ssh
       zen.programs.cli.yazi
       zen.programs.cli.youtube-cli
-      zen.programs.desktop.shojiwm
       zen.programs.desktop.sway
       zen.programs.editors.helix
       zen.programs.editors.opencode
