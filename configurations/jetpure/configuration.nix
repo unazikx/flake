5b91ae2bc4a8422e720e6e4a0db3f60c7aa947a8
unazikx <xfalwa@gmail.com>
2026-07-24 17:03:27 +0900

    jetpure: fix

--- configurations/jetpure/configuration.nix
+++ configurations/jetpure/configuration.nix
@@ -45,7 +45,6 @@
       zen.games.xbox.driver
       zen.hardware.cpu-gpu
       zen.hardware.mounting
-      zen.miscellaneous.chaotic-nyx
       zen.miscellaneous.disko
       zen.miscellaneous.minimal
       zen.miscellaneous.nix
@@ -70,7 +69,6 @@
     includes = [
       # keep-sorted start
       zen.games.gale
-      zen.games.jovian-nixos
       zen.games.heroic
       zen.games.minecraft.prismlauncher
       zen.games.steam
