d405c6960b888f76127b64e6b2ca203168e3d0c4
unazikx <xfalwa@gmail.com>
2026-08-03 22:25:26 +0900

    flake: substituters clean

--- modules/miscellaneous/nix/substituters.nix
+++ modules/miscellaneous/nix/substituters.nix
@@ -16,16 +16,10 @@
             "https://mirror.yandex.ru/nixos"
             "https://install.determinate.systems"
             # keep-sorted start
-            "https://freesmlauncher.cachix.org"
-            "https://lunaislazier.cachix.org"
             "https://niri.cachix.org"
             "https://nix-gaming.cachix.org"
-            "https://nixos-millennium.cachix.org"
-            "https://unmojang.cachix.org"
             "https://vicinae.cachix.org"
-            "https://winapps.cachix.org"
             "https://xache.cachix.org"
-            "https://ytsub.cachix.org"
             # keep-sorted end
           ];
 
@@ -33,16 +27,10 @@
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
             # keep-sorted start
-            "freesmlauncher.cachix.org-1:Jcp5Q9wiLL+EDv8Mh7c6L9xGk+lXr7/otpKxMOuBuDs="
-            "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
             "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
             "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-            "nixos-millennium.cachix.org-1:AaMK3uqfgzCUpjs7+gdHTwwaqkT/vvLMCnUKSY37QAQ="
-            "unmojang.cachix.org-1:OfHnbBNduZ6Smx9oNbLFbYyvOWSoxb2uPcnXPj4EDQY="
             "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-            "winapps.cachix.org-1:HI82jWrXZsQRar/PChgIx1unmuEsiQMQq+zt05CD36g="
             "xache.cachix.org-1:InoMKnvFvDh+J5gFNHN1mmUONxvYJqFImLJY8/bl9YA="
-            "ytsub.cachix.org-1:+//b4AUWp/46hIbTHVLhCU+DIP7TowiUWzM6Hsn8Ryg="
             # keep-sorted end
           ];
         };
