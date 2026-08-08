46fe1aca8ec075edf482d89837be90a3545c033e
unazikx <xfalwa@gmail.com>
2026-08-08 14:06:39 +0900

    flake: fix

--- modules/parts/denful.nix
+++ modules/parts/denful.nix
@@ -79,6 +79,20 @@
     inputs.flake-parts.flakeModules.modules
   ];
 
+  den.default = {
+    includes = [
+      # http://den.denful.dev/reference/batteries
+      # keep-sorted start
+      den.batteries.define-user
+      den.batteries.host-aspects
+      den.batteries.hostname
+      den.batteries.inputs'
+      den.batteries.primary-user
+      den.batteries.self'
+      # keep-sorted end
+    ];
+  };
+
   perSystem =
     {
       pkgs,
