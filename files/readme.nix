dfa9faae888deb16b041a262a509a9c7039bdb32
unazikx <xfalwa@gmail.com>
2026-08-10 12:10:36 +0000

    files: update

--- files/readme.nix
+++ files/readme.nix
@@ -12,25 +12,10 @@
         file."readme.md" = {
           text = ''
             <div align="center">
-              <h1>【 nixos configuration 】</h1>
+              <h1>【 flake configurations 】</h1>
             </div>
 
-            ### structure:
-
-            |                 path | description                                    |
-            | -------------------: | :--------------------------------------------- |
-            |      **./flake.nix** | _inputs_ and _flake-parts_ outputs import-tree |
-            | **./configurations** | declare homes, hosts and users                 |
-            |        **./modules** | modules, aspects and other utils               |
-            |          **./parts** | _flake-parts_ tools and devshells              |
-
-            ### todo:
-
-            1. move _flake.nix_ to _npins_
-            2. add _schema_ for _[nvf](https://nvf.notashelf.dev/)_
-            3. spizdit mnogo moduley u drugih
-
-            ### [previous flake structure](https://github.com/unazikx/flake/tree/4b5dfcc5da3f69a271e8a1d4e4d5479b14f699cc)
+            ### [Previous flake structure](https://github.com/unazikx/flake/tree/4b5dfcc5da3f69a271e8a1d4e4d5479b14f699cc)
           '';
         };
       };
