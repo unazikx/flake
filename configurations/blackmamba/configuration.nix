82bdecf732423270a6a651abf5656b0c2467e884
unazikx <xfalwa@gmail.com>
2026-08-11 10:56:47 +0000

    blackmamba: added info

--- configurations/blackmamba/configuration.nix
+++ configurations/blackmamba/configuration.nix
@@ -11,6 +11,7 @@
     isInstaller = false;
     sopsKey = "age1334jkz6qgtwqctj20zct5ha8psd5ds5yz5n6frxrrwj74l8jcg3qeywx0a";
     drive = toString /dev/disk/by-id/nvme-RPFTJ256PDD2MWX_SS0R27339Z1CD95Z1273;
+    # https://www.impactcomputers.com/hard-drives-ssd/union-memory-rpftj256pdd2mwx
 
     flakeDir = toString /etc/nixos;
     defaultUser = "hackerx";
