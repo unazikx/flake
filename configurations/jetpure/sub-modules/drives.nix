0cbc833753a29eefe69f053af94c686edcb5e0d7
unazikx <xfalwa@gmail.com>
2026-08-12 21:08:48 +0900

    drives: fix

--- configurations/jetpure/sub-modules/drives.nix
+++ configurations/jetpure/sub-modules/drives.nix
@@ -6,24 +6,26 @@
   zen.hosts.jetpure = {
     nixos =
       {
-        lib,
         ...
       }:
       {
-        fileSystems = lib.listToAttrs (
-          lib.map
-            (drive: {
-              name = "/media/${drive}";
-              value = {
-                device = "/dev/disk/by-label/${drive}";
-                options = [ "x-gvfs-show" ];
-              };
-            })
-            [
-              "fatKartman"
-              "fastRider"
-            ]
-        );
+        fileSystems = {
+          "fatKartman" = {
+            device = "/dev/disk/by-label/fatKartman";
+            fsType = "ext4";
+            options = [
+              "x-gvfs-show"
+            ];
+          };
+
+          "fastRider" = {
+            device = "/dev/disk/by-label/fastRider";
+            fsType = "ext4";
+            options = [
+              "x-gvfs-show"
+            ];
+          };
+        };
       };
   };
 }
