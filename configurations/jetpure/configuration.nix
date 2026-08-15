a42fd646dd460d4ac0eb43a3a6525f3636b28673
unazikx <xfalwa@gmail.com>
2026-08-15 18:24:15 +1000

    jetpure: remove strom

--- configurations/jetpure/configuration.nix
+++ configurations/jetpure/configuration.nix
@@ -83,7 +83,6 @@
       zen.games.hytale.launcher
       zen.games.minecraft.prismlauncher
       zen.games.steam
-      zen.games.strom
       zen.games.umu-launcher
       zen.miscellaneous.nix
       zen.miscellaneous.users
