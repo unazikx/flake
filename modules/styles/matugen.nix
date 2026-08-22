71c4810035bddfa71253b4097746e2b21af26f4d
unazikx <xfalwa@gmail.com>
2026-08-22 10:50:24 +0900

    matugen: update

--- modules/styles/matugen.nix
+++ modules/styles/matugen.nix
@@ -18,6 +18,7 @@
     nixos =
       {
         inputs,
+        pkgs,
         ...
       }:
       {
@@ -29,15 +30,15 @@
           enable = true;
 
           mode = "dark";
-          seedColor = "a988b0";
+          # seedColor = "a988b0";
 
-          # wallpaper = {
-          #   value = "value";
-          #   source = pkgs.fetchurl {
-          #     url = "https://w.wallhaven.cc/full/og/wallhaven-ogl5z9.png";
-          #     sha256 = "sha256-VvnBCulYtyVqJOQ5psLJyIvK8YffWPP8Stz9KgznGcY=";
-          #   };
-          # };
+          wallpaper = {
+            prefer = "value";
+            source = pkgs.fetchurl {
+              url = "https://w.wallhaven.cc/full/og/wallhaven-ogl5z9.png";
+              sha256 = "sha256-VvnBCulYtyVqJOQ5psLJyIvK8YffWPP8Stz9KgznGcY=";
+            };
+          };
         };
       };
   };
