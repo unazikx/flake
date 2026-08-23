f365121ab2dbe2172fead9f4c58d3a395082b91b
unazikx <xfalwa@gmail.com>
2026-08-23 22:59:39 +0900

    packages: piper fix

--- packages/yazi-plugins/piper.nix
+++ packages/yazi-plugins/piper.nix
@@ -4,15 +4,15 @@
   lib,
 }:
 
-yaziPlugins.mkYaziPlugin {
+yaziPlugins.mkYaziPlugin (_old: {
   pname = "piper";
   version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "alberti42";
     repo = "faster-piper.yazi";
-    rev = "8b794bfa3bc9c780e3f03b6f5a0ccde7744e54bb";
-    hash = "sha256-m6ZiwA36lcdZORK3KIz4Xq3bs7mmtC6j62B/+BuDGAQ=";
+    tag = "v${_old.version}";
+    hash = "sha256-a7/KTIoIU9idxhYmYFsp6/ezmiBK/mEYfEz9zqZZiEU=";
   };
 
   meta = {
@@ -20,4 +20,4 @@ yaziPlugins.mkYaziPlugin {
     homepage = "https://github.com/alberti42/faster-piper.yazi";
     license = lib.licenses.mit;
   };
-}
+})
