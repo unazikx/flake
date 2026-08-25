0a1979d01376d2bf7b8f18442a46ede167922cae
unazikx <xfalwa@gmail.com>
2026-08-25 18:04:09 +0900

    system: add nixos tags

--- modules/miscellaneous/version.nix
+++ modules/miscellaneous/version.nix
@@ -7,10 +7,12 @@
   zen.miscellaneous.version = {
     os =
       {
+        config,
         ...
       }:
       {
         system.stateVersion = lib.mkDefault "26.05";
+        system.nixos.tags = [ config.networking.hostName ];
       };
 
     homeManager =
