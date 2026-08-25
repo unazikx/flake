0b7fb03c03a3653054c8c52ee47e3af7e859f3c1
unazikx <xfalwa@gmail.com>
2026-08-25 13:36:03 +0900

    btop: added cap for users

--- modules/programs/cli/monitor.nix
+++ modules/programs/cli/monitor.nix
@@ -3,33 +3,62 @@
 }:
 
 {
-  zen.programs.cli.monitor = {
-    description = ''
-      monitor system status
-      CPU, RAM, NETWWORK, GPU...
-    '';
+  zen.programs.cli.monitor =
+    {
+      config,
+      ...
+    }:
+    {
+      description = ''
+        monitor system status
+        CPU, RAM, NETWWORK, GPU...
+      '';
 
-    homeManager =
-      {
-        pkgs,
-        ...
-      }:
-      {
-        home.packages = [
-          pkgs.btop
-          pkgs.nvtopPackages.amd
-        ];
+      meta = {
+        btop = pkgs: pkgs.btop;
+      };
 
-        programs.btop = {
-          enable = true;
-
-          settings = {
-            update_ms = 100;
-            theme_background = true;
-            vim_keys = true;
-            rounded_corners = true;
+      nixos =
+        {
+          pkgs,
+          lib,
+          ...
+        }:
+        {
+          security.wrappers.btop = {
+            owner = "root";
+            group = "root";
+            capabilities = lib.concatStringsSep "," [
+              "cap_perfmon"
+              "cap_sys_admin"
+              "cap_net_raw+ep"
+            ];
+            source = lib.getExe (config.meta.btop pkgs);
           };
         };
-      };
-  };
+
+      homeManager =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          home.packages = [
+            pkgs.nvtopPackages.amd
+          ];
+
+          programs.btop = {
+            enable = true;
+
+            package = config.meta.btop pkgs;
+
+            settings = {
+              update_ms = 100;
+              theme_background = true;
+              vim_keys = true;
+              rounded_corners = true;
+            };
+          };
+        };
+    };
 }
